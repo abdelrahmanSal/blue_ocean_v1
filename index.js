60171145425cee27f7c11ca28de936b3450e0246
abdelrahman saleh <156146452+abdelrahmanSal@users.noreply.github.com>
2024-05-26 04:56:32 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,53 +1,55 @@
-// Telegram Bot API token
-const TELEGRAM_BOT_TOKEN = "7167739580:AAEyo62W9zghyPQCKVGkNn1nF0BcggEuYV4";
+let face = document.querySelector("#facebook_img");
 
-// Telegram chat ID (where you want to receive messages)
-const TELEGRAM_CHAT_ID = "5908974662";
+face.addEventListener("click", () => {
+  window.open("https://me-qr.com/lePHfBQo");
+});
 
+let ins = document.querySelector("#instagram_img");
+
+ins.addEventListener("click", () => {
+  window.open("https://me-qr.com/fnIJKGbg");
+});
+
+let tel = document.querySelector("#telegram_img");
+
+tel.addEventListener("click", () => {
+  window.open("https://me-qr.com/BuESNvNX");
+});
+
+let what = document.querySelector("#whats_img");
+
+what.addEventListener("click", () => {
+  window.open("https://me-qr.com/vsrvpoJW");
+});
+
+//  function dev(userName,age){
+//     return userName + age
+
+//  }
+
+// console.log( dev("mosfata ",20))
+
+// // let fn = ()=>{
+// //     alert("asdasd")
+// // }
+
+// // fn()
+
+// (function name(){
+//     alert("asdasd")
+// })()
 document
-  .getElementById("loginForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent the form from submitting normally
-
-    // Get form values
-    var name = document.getElementById("name").value;
-    var number = document.getElementById("number").value;
-    var email = document.getElementById("email").value;
-
-    // Construct message
-    var message = "Name: " + name + "\n";
-    message += "Phone Number: " + number + "\n";
-    message += "Email: " + email + "\n";
-
-    // Send message to Telegram
-    sendMessageToTelegram(message);
-
-    // Clear the form fields (optional)
-    document.getElementById("name").value = "";
-    document.getElementById("number").value = "";
-    document.getElementById("email").value = "";
-
-    // Optionally, you can show a success message to the user
-    alert("Hi " + name + "in blue ocean web ");
-
-    window.location.href = "./index1.html";
-  });
-
-function sendMessageToTelegram(message) {
-  // URL for sending messages via Telegram Bot API
-  const TELEGRAM_API_URL = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
-
-  fetch(TELEGRAM_API_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      chat_id: TELEGRAM_CHAT_ID,
-      text: message,
-    }),
-  })
-    .then((response) => response.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.error("Error sending message:", error));
-}
+      .getElementById("btnperfume")
+      .addEventListener("click", function () {
+        window.location.href = "./perfume.html"; // Make sure index.html is in the same directory
+      });
+document
+      .getElementById("btnbody")
+      .addEventListener("click", function () {
+        window.location.href = "./skincare-web.html"; // Make sure index.html is in the same directory
+      });
+document
+      .getElementById("btnoffers")
+      .addEventListener("click", function () {
+        window.location.href = "./offers.html"; // Make sure index.html is in the same directory
+      });
